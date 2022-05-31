5b5393ad297730d5a7895484c10059c4addb6d23
C.Solovev <constantine.solovev@gmail.com>
2022-05-31 07:59:11 +0400

    2 month challenge is completed

---
+++ src/leetcode/check_if_strig_contains_all_binary_codes_of_size_k.rs
@@ -0,0 +1,39 @@
+//! 1461. Check If a String Contains All Binary Codes of Size K
+
+use std::collections::HashSet;
+
+impl Solution {
+
+    // array (fast) space 0(2^k)
+    pub fn has_all_codes(s: String, k: i32) -> bool {
+        let k = k as usize;
+        if s.len() < 2 { return false }
+        if k > s.len() { return false }
+        let target = 1 << k;
+        let mut got = vec![false; target];
+
+        for idx in 0..=s.len()-k {
+            let cur = usize::from_str_radix(&s[idx..idx + k], 2).unwrap();
+            got[cur] = true;
+        }
+
+        got.into_iter().filter(|n| *n).count() == target // 2^k
+    }
+
+    // hashset (slow), space O(2^k)
+    pub fn has_all_codes_(s: String, k: i32) -> bool {
+        let k = k as usize;
+        if s.len() < 2 { return false }
+        if k > s.len() { return false }
+
+        let mut buf = HashSet::new();
+        for idx in 0..=s.len()-k {
+            let cur = (&s[idx..idx + k]).to_string();
+            buf.insert(cur);
+        }
+
+        buf.len() == 1 << k // 2^k
+    }
+}
+
+struct Solution;
--- src/leetcode/mod.rs
+++ src/leetcode/mod.rs
@@ -297,6 +297,8 @@ mod russian_doll_envelopes;
 mod longest_valid_parentheses;
 /// 201. Bitwise AND of Numbers Range
 mod bitwise_and_of_numver_range;
+/// 318. Maximum Product of Word Lengths
+mod maximum_product_of_word_lengths;
 /// 752. Open the Lock
 mod open_the_lock;
 /// 279. Perfect Squares
@@ -311,6 +313,8 @@ mod number_of_steps_to_reduce_number_to_zero;
 mod missing_number;
 /// 318. Maximum Product of Word Lengths
 mod maximum_product_of_word_lengths;
+/// 1461. Check If a String Contains All Binary Codes of Size K
+mod check_if_strig_contains_all_binary_codes_of_size_k;
 
 
 
